8c9fb3a6b160c274b226cea427f9fea3f783b44b
JoffreyVC <joffreyvancauwenberghe@gmail.com>
2022-12-18 10:53:33 +0100

    comments Jof

--- sbuffer.c
+++ sbuffer.c
@@ -17,33 +17,31 @@
 #include <stdlib.h>
 #include <sys/types.h>
 
+
 // ^ Element in de Buffer
 typedef struct sbuffer_node {
-    // stond eerst een link naar de vorige node maar dat werkt in ons nadeel als we altijd van langs achter willen kijken
-    struct sbuffer_node* next;   // pointer naar de volgende
-    sensor_data_t data;          // data in een node van de buffer
-    bool stored;          // is storage manager al geweest?
-    bool datamanaged;     // is data manager al geweest?
-    pthread_mutex_t node_mutex;  // mutex nodig om te kijken of beiden booleans true zijn
+    struct sbuffer_node* next;          // pointer naar de volgende
+    sensor_data_t data;                 // data in een node van de buffer
+    bool stored;                        // is storage manager al geweest?
+    bool datamanaged;                   // is data manager al geweest?
+    pthread_mutex_t node_mutex;         // mutex nodig om te kijken of beiden booleans true zijn
 } sbuffer_node_t;
 
 // ^ De Buffer
 struct sbuffer {
-    sbuffer_node_t* head;  // kop waar elementen worden toegevoegd door connection manager
-    sbuffer_node_t* tail;  // staart waar readers elementen van halen
+    sbuffer_node_t* head;               // kop waar elementen worden toegevoegd door connection manager
+    sbuffer_node_t* tail;               // staart waar readers elementen van halen
     bool closed;
     pthread_rwlock_t rw_lock;
-    unsigned long id_readers[2];  // storage, data - manager
-    pthread_cond_t bufferIsExpanded;  // condition variabele waarop we kunnen wachten
-    pthread_mutex_t mutex;  // hulp voor condition variabele ... ambetant maar verplicht ...
+    unsigned long id_readers[2];        // storage, data - manager
+    pthread_cond_t bufferIsExpanded;    // condition variabele waarop we kunnen wachten
+    pthread_mutex_t mutex;              // hulp voor condition variabele ... ambetant maar verplicht ...
 };
 
-// // todo https://stackoverflow.com/questions/2699993/using-pthread-condition-variable-with-rwlock dit is fucked but there is no other way
 
-// ---------------------------------------------------------  MAKEN EN INITIALISEREN  ----------------------------------------------------------
 
-// maak nodekkk
-static sbuffer_node_t* create_node(const sensor_data_t* data) {     // maak een node voor in de buffer
+// maakt node
+static sbuffer_node_t* create_node(const sensor_data_t* data) {     
     sbuffer_node_t* node = malloc(sizeof(*node));
     *node = (sbuffer_node_t){
         .data = *data,
@@ -51,25 +49,27 @@ static sbuffer_node_t* create_node(const sensor_data_t* data) {     // maak een 
         .stored = false,
         .datamanaged = false
     };
-    ASSERT_ELSE_PERROR(pthread_mutex_init(&node->node_mutex, NULL) == 0);  // initialisatie van mutex per node
+    ASSERT_ELSE_PERROR(pthread_mutex_init(&node->node_mutex, NULL) == 0);               // initialisatie van mutex per node
     return node;
 }
 
 // maak buffer
 sbuffer_t* sbuffer_create() {
     sbuffer_t* buffer = malloc(sizeof(sbuffer_t));
-    assert(buffer != NULL);  // should never fail due to optimistic memory allocation
+    assert(buffer != NULL);                                                             // should never fail due to optimistic memory allocation
 
     buffer->head = NULL;
     buffer->tail = NULL;
     buffer->closed = false;
 
-    ASSERT_ELSE_PERROR(pthread_rwlock_init(&buffer->rw_lock, NULL) == 0);  // initialisatie RWlock
-    ASSERT_ELSE_PERROR(pthread_cond_init(&(buffer->bufferIsExpanded), NULL) == 0);  // initialisatie CD variabele om wait en signal uit te voeren
-    ASSERT_ELSE_PERROR(pthread_mutex_init(&buffer->mutex, NULL) == 0);  // initialisatie onbelangrijke mutex
+    ASSERT_ELSE_PERROR(pthread_rwlock_init(&buffer->rw_lock, NULL) == 0);               // initialisatie RWlock
+    ASSERT_ELSE_PERROR(pthread_cond_init(&(buffer->bufferIsExpanded), NULL) == 0);      // initialisatie CD variabele om wait en signal uit te voeren
+    ASSERT_ELSE_PERROR(pthread_mutex_init(&buffer->mutex, NULL) == 0);                  // initialisatie onbelangrijke mutex
 
     return buffer;
 }
+
+//printen van de buffer
 void printBuffer(sbuffer_t* buffer) {
     if (buffer->head == NULL) {
         printf("Buffer is leeg");
@@ -85,28 +85,14 @@ void printBuffer(sbuffer_t* buffer) {
     }
 }
 
-
-
-// verwijder buffer
-void sbuffer_destroy(sbuffer_t* buffer) {       // vrijgeven van de buffer
+// vrijgeven van de buffer
+void sbuffer_destroy(sbuffer_t* buffer) {       
     assert(buffer);
-    // make sure it's empty
     assert(buffer->head == buffer->tail);
-    // // ASSERT_ELSE_PERROR(pthread_mutex_destroy(&buffer->mutex) == 0);
     free(buffer);
 }
 
-/* -> verwoven in de nodige functies zelf
-void sbuffer_lock(sbuffer_t* buffer) {      // lock de buffer
-    assert(buffer);
-    // // ASSERT_ELSE_PERROR(pthread_mutex_lock(&buffer->mutex) == 0);
-}
-void sbuffer_unlock(sbuffer_t* buffer) {        // unlock de buffer
-    assert(buffer);
-    // // ASSERT_ELSE_PERROR(pthread_mutex_unlock(&buffer->mutex) == 0);
-}
-*/
-
+//Hier worden de id's van de managerthreads opgeslagen
 void sbuffer_identify_readers(sbuffer_t* buffer, bool isDataManager){
     if(isDataManager) buffer->id_readers[1] = pthread_self();
     else buffer->id_readers[0] = pthread_self();
@@ -124,8 +110,6 @@ void sbuffer_close(sbuffer_t* buffer) {     // close de buffer
     pthread_rwlock_unlock(&(buffer->rw_lock));
 }
 
-// ---------------------------------------------------------  CHECKS  ----------------------------------------------------------
-
 
 bool sbuffer_is_empty(sbuffer_t* buffer) {      // is de buffer leeg?
     assert(buffer);
@@ -137,50 +121,41 @@ bool sbuffer_is_closed(sbuffer_t* buffer) {     // is de buffer closed?
     return buffer->closed;
 }
 
-// ---------------------------------------------------------  TOEVOEGEN  ----------------------------------------------------------
-//f
 
-int sbuffer_insert_first(sbuffer_t* buffer, sensor_data_t const* data) {        // insert aan het begin van de buffer
-    assert(buffer && data);
-    // ? de check hoeft denk ik niet met synchronisatie te gebeuren
-    if (buffer->closed)
-        return SBUFFER_FAILURE;  // kan niet als hij toe is
+// insert aan het begin van de buffer
+int sbuffer_insert_first(sbuffer_t* buffer, sensor_data_t const* data) {        
+    assert(buffer && data);                             // ? de check hoeft denk ik niet met synchronisatie te gebeuren
+    
+    if (buffer->closed)                                 // kan niet als hij toe is
+        return SBUFFER_FAILURE;  
 
-    // ? de nieuwe node maken kan ook zonder synchronisatie gebeuren denk ik
     sbuffer_node_t* node = create_node(data);
     assert(node->next == NULL);
 
     pthread_rwlock_wrlock(&(buffer->rw_lock));
     pthread_mutex_lock(&(buffer->mutex));
 
-    // insert it - we zijn alleen in de buffer
-    // we voegen onze nieuwe node toe aan de kop van de buffer
     if (buffer->head != NULL)
         buffer->head->next = node;
     buffer->head = node;
     if (buffer->tail == NULL)
         buffer->tail = node;
 
-    //printf("einde insert \n");
     printBuffer(buffer);
     printf("\n");
 
     // geef rw lock terug vrij
     pthread_mutex_unlock(&(buffer->mutex));
     pthread_rwlock_unlock(&(buffer->rw_lock));
-    //printf("broadcast \n");
     pthread_cond_broadcast(&(buffer->bufferIsExpanded));  // maak de readers wakker die hier vastzaten
 
     return SBUFFER_SUCCESS;
 }
 
-// ---------------------------------------------------------  VERWIJDEREN  ----------------------------------------------------------
 
-
-sensor_data_t sbuffer_remove_last(sbuffer_t* buffer) {      // geef laatste van buffer terug en verwijder
+// geef laatste van buffer terug en verwijder
+sensor_data_t sbuffer_remove_last(sbuffer_t* buffer) {      
     assert(buffer);
-    // assert(buffer->head != NULL);
-    // kan blocken, maar we moeten het zeker hebben ... - alternatief is met de try functie
     pthread_rwlock_rdlock(&(buffer->rw_lock));
     bool isDataManager = pthread_self() == buffer->id_readers[1];
     bool isStorageManager = !isDataManager;
@@ -203,29 +178,23 @@ sensor_data_t sbuffer_remove_last(sbuffer_t* buffer) {      // geef laatste van 
                 if (gevonden){
                     if (pthread_mutex_trylock(&(used_node->node_mutex)) != 0) {
                         gevonden = false;                                       
-                    } else {
-                        // printf("we hebben een node gevonden en gelockt %d \n", isDataManager);
-                    }
+                    } 
                 } 
                 if (used_node->next == NULL){
                     break;
                 }
                 if (!gevonden) used_node = used_node->next;
-
-
             } 
         }
 
         // er is geen node meer om te lezen -> slaap en geef lock terug
         if (!gevonden){
-            //printf("we gaan even wachten %d \n", isDataManager);
             pthread_mutex_lock(&(buffer->mutex));
 
             // geef rw lock terug vrij
             pthread_rwlock_unlock(&(buffer->rw_lock));
             pthread_cond_wait(&(buffer->bufferIsExpanded), &(buffer->mutex));
 
-            //printf("ik ben terug wakker %d \n", isDataManager);
             pthread_mutex_unlock(&(buffer->mutex));
             pthread_rwlock_rdlock(&(buffer->rw_lock));
         }
@@ -236,11 +205,14 @@ sensor_data_t sbuffer_remove_last(sbuffer_t* buffer) {      // geef laatste van 
     if (isDataManager) used_node->datamanaged = true;
     else used_node->stored = true;
 
-    printf("%d %d \n", used_node->stored, used_node->datamanaged);
+    //printf("%d %d \n", used_node->stored, used_node->datamanaged);
 
     // geef node en buffer vrij
     pthread_mutex_unlock(&(used_node->node_mutex));
     pthread_rwlock_unlock(&(buffer->rw_lock));
+
+    printBuffer(buffer);
+    printf("\n");
 
     if (used_node->stored && used_node->datamanaged){
         // neem write lock voor verwijderen
@@ -257,13 +229,7 @@ sensor_data_t sbuffer_remove_last(sbuffer_t* buffer) {      // geef laatste van 
         }
         temp->next = NULL;
         free(temp);
-
-        // Geef buffer terug vrij
-        printf("einde verwijderen \n");
         pthread_rwlock_unlock(&(buffer->rw_lock));
     } 
-
-    //printf("einde verwijderen \n");
-
     return ret;
 }
